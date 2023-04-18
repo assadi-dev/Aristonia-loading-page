98eefce65f2811c608b7921bad1c197afc1f7ef3
Assadi <62776480+assadi-dev@users.noreply.github.com>
2023-04-18 03:28:08 +0200

    ajout handler

--- js/main.js
+++ js/main.js
@@ -6,6 +6,9 @@ const objectNode = {
   volumeSoundBar: document.querySelector(".volume-sounBar-container"),
   volumeCursor: document.querySelector("#sound_cursor"),
   volumeLevel: document.querySelector("#volume-level"),
+  audio: document.querySelector(".audio"),
+  progressPercent: document.querySelector(".progress-percent"),
+  progressBar: document.querySelector(".bar"),
 };
 const svgVolumePath = {
   off: "../assets/icon/sound-off.svg",
@@ -19,12 +22,13 @@ let positionCusorVol = 293;
 
 document.addEventListener("DOMContentLoaded", () => {
   load_volume_icone();
-  objectNode.video.volume = 0;
   //Mouvemen du curseur bar de son
   objectNode.volumeSoundBar.addEventListener("mousedown", active_move);
   objectNode.volumeSoundBar.addEventListener("mouseup", leave_move);
   objectNode.volumeSoundBar.addEventListener("mouseleave", leave_move);
   objectNode.volumeSoundBar.addEventListener("mousemove", move_volume_cursor);
+  //Handler Progress
+  handlerProgress();
 });
 
 const load_volume_icone = async (e) => {
@@ -34,23 +38,28 @@ const load_volume_icone = async (e) => {
   objectNode.volumeOff.classList.add("volume-selected");
   objectNode.volumeOn.onclick = play_video;
   objectNode.volumeOff.onclick = stop_video;
+  objectNode.audio.volume = 0;
 };
 
 const play_video = async (e) => {
   const icon = new Image();
   icon.src = svgVolumePath.on;
   const clientWidth = objectNode.volumeSoundBar.clientWidth;
-  positionCusorVol = clientWidth / 2;
+  //positionCusorVol = clientWidth / 2;
 
   objectNode.volumeOff.classList.remove("volume-selected");
   objectNode.volumeOn.classList.add("volume-selected");
   objectNode.volumeIcon.replaceChildren(icon);
-  objectNode.video.volume = volumeLevel;
+
   objectNode.volumeLevel.textContent = `${volumeComplete}%`;
   objectNode.volumeCursor.style.transform = `translateX(${positionCusorVol}px)`;
+  objectNode.audio.volume = volumeLevel;
 
   if (objectNode.video.paused || objectNode.video.ended) {
     objectNode.video.play();
+  }
+  if (objectNode.audio.paused || objectNode.audio.ended) {
+    objectNode.audio.play();
   }
 };
 
@@ -60,7 +69,7 @@ const stop_video = async () => {
   objectNode.volumeOn.classList.remove("volume-selected");
   objectNode.volumeOff.classList.add("volume-selected");
   objectNode.volumeIcon.replaceChildren(icon);
-  objectNode.video.volume = 0;
+  objectNode.audio.volume = 0;
 };
 
 const ended_video = (e) => {
@@ -73,6 +82,15 @@ const ended_video = (e) => {
 const active_move = (e) => {
   e.stopPropagation();
   clicked = true;
+  objectNode.volumeOff.classList.remove("volume-selected");
+  objectNode.volumeOn.classList.add("volume-selected");
+  const icon = new Image();
+  icon.src = svgVolumePath.on;
+  objectNode.volumeIcon.replaceChildren(icon);
+
+  if (objectNode.audio.paused || objectNode.audio.ended) {
+    objectNode.audio.play();
+  }
 };
 const leave_move = (e) => {
   e.stopPropagation();
@@ -84,17 +102,51 @@ const move_volume_cursor = (e) => {
   }
   const mousePose = e.layerX;
   const clientWidth = objectNode.volumeSoundBar.clientWidth;
-  const limit = clientWidth - 18;
+  const limit = clientWidth - 8;
   let percent = Math.floor((mousePose * 100) / clientWidth);
   volumeLevel = percent * 0.01;
-  volumeComplete = Number(volumeLevel.toFixed(1)) * 100;
+  volumeComplete = percent;
 
   if (mousePose > 1 && mousePose < limit) {
     positionCusorVol = mousePose;
     objectNode.volumeCursor.style.transform = `translateX(${
       positionCusorVol - 9
     }px)`;
-    objectNode.video.volume = volumeLevel;
+    objectNode.audio.volume = volumeLevel;
     objectNode.volumeLevel.textContent = `${volumeComplete}%`;
   }
+};
+
+const handlerProgress = () => {
+  var count = 0;
+  var thisCount = 0;
+
+  const handlers = {
+    startInitFunctionOrder(data) {
+      count = data.count;
+    },
+
+    initFunctionInvoking(data) {
+      let progress = (data.idx / count) * 100 + "%";
+      objectNode.progressPercent.textContent = progress;
+      objectNode.progressBar.style.width = progress;
+    },
+
+    startDataFileEntries(data) {
+      count = data.count;
+    },
+
+    performMapLoadFunction(data) {
+      ++thisCount;
+
+      let progress = (thisCount / count) * 100 + "%";
+
+      objectNode.progressPercent.textContent = progress;
+      objectNode.progressBar.style.width = progress;
+    },
+  };
+
+  window.addEventListener("message", function (e) {
+    (handlers[e.data.eventName] || function () {})(e.data);
+  });
 };
